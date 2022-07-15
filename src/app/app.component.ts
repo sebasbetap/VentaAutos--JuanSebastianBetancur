c0aa35a8ef00fc1ff9997fbb1752c3ec661878cb
Juan Sebastian Betancur Posada <jbetancu@bancolombia.com.co>
2022-07-15 09:24:23 -0500

    Cars-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'VentaAutos';
+  titulo: string = "Venta de Autos";
+  pais: string = "Colombia";
 }
