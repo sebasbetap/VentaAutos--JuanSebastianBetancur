ef209661e04a6d3e0324fa3b4e5565825bc89f21
Juan Sebastian Betancur Posada <jbetancu@bancolombia.com.co>
2022-07-26 15:13:37 -0500

    Desarrollo-prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AEspacioPipe } from 
 import { ListaAutosComponent } from './autos/lista-autos/lista-autos.component';
+import { EstrellasComponent } from './shared/estrellas/estrellas.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ListaAutosComponent
+    ListaAutosComponent, EstrellasComponent
   ],
   imports: [
     BrowserModule,
