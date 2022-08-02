7b95c8a28d1a1e6c4acf32501311932641e4ff93
Juan Sebastian Betancur Posada <jbetancu@bancolombia.com.co>
2022-08-02 17:48:05 -0500

    App-Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AEspacioPipe } from 
 import { ListaAutosComponent } from './autos/lista-autos/lista-autos.component';
 import { EstrellasComponent } from './shared/estrellas/estrellas.component';
+import { AEspacioPipe } from './shared/a-espacio.pipe';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgModel } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ListaAutosComponent, EstrellasComponent
+    ListaAutosComponent, 
+    EstrellasComponent, 
+    AEspacioPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/autos/lista-autos/lista-autos.component.ts
+++ src/app/autos/lista-autos/lista-autos.component.ts
@@ -1,12 +1,110 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { Auto } from "src/app/datos/auto";
 
 @Component ({
     selector: "lista-autos",
-    templateUrl: "./lista-autos.component.html"
+    templateUrl: "./lista-autos.component.html",
 })
 
-export class ListaAutosComponent {
-    tituloListaAutos: string = "Lista de Automóviles";
+export class ListaAutosComponent implements OnInit {
 
-    arregloAutos: string[] = ["uno"];
+    faStar = faStar;
+
+    starsList = [];
+    tituloListaAutos: string = "Lista de Autos";
+
+    listaAutos: Auto[] = [];
+    listaAutosFiltrados: Auto[] = [];
+
+    imageWidth = 200;
+    imageMargin = 2;
+    muestraImagen: boolean = false;
+    
+    private _filtro: string= "";
+
+    get filtro(): string {
+        return this._filtro;
+    }
+
+    set filtro(filtro: string) {
+        this._filtro = filtro;
+        this.filtraAutos(filtro);
+    }
+
+    filtraAutos( filtrarPor: string): void {
+        filtrarPor = filtrarPor.toLocaleLowerCase();
+        this.listaAutosFiltrados = this.listaAutos.filter(
+            ( auto: Auto ) => auto.marca.toLocaleLowerCase().includes( filtrarPor )
+        )
+    }
+
+    ngOnInit(): void {
+        this.listaAutos = [
+            {
+                id: 1,
+                marca: "Porsche",
+                modelo: "Taycan",
+                anio: 2022,
+                color: "Azul Claro",
+                kilometros: 0,
+                calificacion: 5,
+                precio: 140345,
+                imagenUrl: "../../assets/1_Porsche_Taycan.webp"
+            },
+            {
+                id: 2,
+                marca: "Tesla",
+                modelo: "Model 3",
+                anio: 2022,
+                color: "Blanco",
+                kilometros: 0,
+                calificacion: 4.5,
+                precio: 56490,
+                imagenUrl: "../../assets/2_Tesla_Model3.webp"
+            },
+            {
+                id: 3,
+                marca: "Hyundai",
+                modelo: "Ioniq 5",
+                anio: 2022,
+                color: "Verde Oscuro",
+                kilometros: 10000,
+                calificacion: 4.4,
+                precio: 54545,
+                imagenUrl: "../../assets/3_Hyundai_Ioniq5.webp"
+            },
+            {
+                id: 4,
+                marca: "Audi",
+                modelo: "e-tron GT",
+                anio: 2020,
+                color: "Azul Negro",
+                kilometros: 30000,
+                calificacion: 3.5,
+                precio: 33302,
+                imagenUrl: "../../assets/4_Audi_etronGT.webp"
+            },
+            {
+                id: 5,
+                marca: "Ford",
+                modelo: "Mustang Mach-E",
+                anio: 2021,
+                color: "Azul Claro",
+                kilometros: 0,
+                calificacion: 2,
+                precio: 21359,
+                imagenUrl: "../../assets/9_Ford_Mustang_MachE.webp"
+            }
+        ];
+        this.listaAutosFiltrados = this.listaAutos;
+    }
+
+    toggleImage(): void {
+        this.muestraImagen = !this.muestraImagen;
+    }
+
+    onClickCalificacion( mensaje: string): void {
+        alert( "Detalle de calificación: "+ mensaje)
+    }
 }
